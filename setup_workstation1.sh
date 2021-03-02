64671d8e7d981b372a573975360d69c12dd0b072
Resistor52 <ken@kennethghartman.com>
2021-03-02 11:13:40 -0500

    deleteed setup_workstation1.sh

--- setup_workstation1.sh
+++
@@ -1,9 +0,0 @@
-## Supply this file as the user data when launching an Ubuntu EC2 instance
-#!/bin/bash
-apt -y update
-DEBIAN_FRONTEND=noninteractive apt -y upgrade
-apt install -y ubuntu-mate-desktop
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
-apt install -y xrdp
-systemctl enable xrdp
